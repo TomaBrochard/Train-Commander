c41dffa62cdc1b83e51bd6e6a61cd2d4cde9aff7
TomaBrochard <toma.brochard@supinfo.com>
2016-05-07 17:17:15 +0100

    Ajoutez des fichiers projet.

---
+++ Train-Commander/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Train_Commander.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Train-Commander/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Train_Commander.WebApiApplication" Language="C#" %>
